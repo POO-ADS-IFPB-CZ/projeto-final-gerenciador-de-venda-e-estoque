2b58b94dff12221c9b1005ffa5e625230820398d
EwertonVeloso <velosoewerton@gmail.com>
2025-08-20 12:23:46 -0300

    feat: cria a classe produto

--- src/main/java/com/poo/projeto/crud/model/Funcionario.java
+++ src/main/java/com/poo/projeto/crud/model/Funcionario.java
@@ -1,4 +1,26 @@
 package com.poo.projeto.crud.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Funcionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long matricula;
+    private String nome;
+    private String cpf;
+    private List<String> telefones;
+    private String cargo;
+    private Double salario;
 }
--- src/main/java/com/poo/projeto/crud/model/Produto.java
+++ src/main/java/com/poo/projeto/crud/model/Produto.java
@@ -1,4 +1,25 @@
 package com.poo.projeto.crud.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+    private String nome;
+    private String descricao;
+    private int  estoque;
+    private Double preco;
+
 }
