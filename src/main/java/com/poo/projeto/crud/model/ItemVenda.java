34f69d4a332af7f11860038faa6a4710725b82be
EwertonVeloso <velosoewerton@gmail.com>
2025-08-20 12:36:26 -0300

    refactor: altera a cardinalidade do atributo produto em ItemVenda

--- src/main/java/com/poo/projeto/crud/model/ItemVenda.java
+++ src/main/java/com/poo/projeto/crud/model/ItemVenda.java
@@ -1,4 +1,27 @@
 package com.poo.projeto.crud.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class ItemVenda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(name = "produto_codigo")
+    @ManyToOne
+    private Produto produto;
+    @ManyToOne
+    private Venda venda;
+
+    private int quantidade;
+    private Double precoUnitario;
+
 }
