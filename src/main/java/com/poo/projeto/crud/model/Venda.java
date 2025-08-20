979dbc0aac330417fbb17cbb88ba97c108f18b80
EwertonVeloso <velosoewerton@gmail.com>
2025-08-20 12:40:10 -0300

    feat: adiciona uma lista de itens a classe Venda

--- src/main/java/com/poo/projeto/crud/model/Venda.java
+++ src/main/java/com/poo/projeto/crud/model/Venda.java
@@ -1,4 +1,29 @@
 package com.poo.projeto.crud.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Venda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String codigo;
+    private LocalDate dataVenda;
+
+    @ManyToOne
+    private Funcionario funcionario;
+
+    @OneToMany(mappedBy = "Venda", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<ItemVenda> itens;
+
 }
