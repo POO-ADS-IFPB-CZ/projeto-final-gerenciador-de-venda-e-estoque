8643b9958fbbb010fd30a4bccaf7e50246362b77
EwertonVeloso <velosoewerton@gmail.com>
2025-08-20 14:17:48 -0300

    feat: Adiciona método de busca de venda por id

--- src/main/java/com/poo/projeto/crud/service/ProdutoService.java
+++ src/main/java/com/poo/projeto/crud/service/ProdutoService.java
@@ -53,7 +53,4 @@ public class ProdutoService {
         produtoRepository.deleteById(id);
     }
 
-
-
-
 }
--- src/main/java/com/poo/projeto/crud/service/VendaService.java
+++ src/main/java/com/poo/projeto/crud/service/VendaService.java
@@ -1,4 +1,35 @@
 package com.poo.projeto.crud.service;
 
+import com.poo.projeto.crud.model.Venda;
+import com.poo.projeto.crud.repository.VendaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class VendaService {
+
+    @Autowired
+    private VendaRepository repository;
+
+    public List<Venda> ListarVendas() {
+        try {
+            return repository.findAll();
+        }catch (Exception e){
+            throw new RuntimeException("Erro ao listar Vendas");
+        }
+    }
+
+    public Venda buscarVendaPorId(Long id) {
+        return repository.findById(id)
+                .orElseThrow( () -> new RuntimeException("Venda não encontrada!"));
+    }
+
+    public Venda cadastrarVenda(Venda venda) {
+        return repository.save(venda);
+    }
+
+
+
 }
