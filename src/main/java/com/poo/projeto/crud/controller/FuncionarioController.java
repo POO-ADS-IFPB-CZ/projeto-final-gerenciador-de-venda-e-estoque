221b337693fce55b76ca0201dff77876b044f132
WesleyyD <delano.wesley@academico.ifpb.edu.br>
2025-08-26 15:47:19 -0300

    feat: Cria metodo para carregar tabela

--- src/main/java/com/poo/projeto/crud/controller/FuncionarioController.java
+++ src/main/java/com/poo/projeto/crud/controller/FuncionarioController.java
@@ -1,7 +1,10 @@
 package com.poo.projeto.crud.controller;
 
+import com.poo.projeto.crud.model.Funcionario;
 import com.poo.projeto.crud.service.FuncionarioService;
 import com.poo.projeto.crud.view.FuncionarioView;
+
+import java.util.List;
 
 public class FuncionarioController {
 
@@ -13,6 +16,15 @@ public class FuncionarioController {
         this.funcionarioService = funcionarioService;
     }
 
+    private void carregarTabela() {
+        view.getTableModel().setRowCount(0);
+        List<Funcionario> funcionarios = funcionarioService.listarFuncionarios();
+        for (Funcionario f : funcionarios) {
+            view.getTableModel().addRow(new Object[]{
+                    f.getId(), f.getNome(), f.getCpf(), f.getMatricula(), f.getCargo(), f.getSalario()
+            });
+        }
+    }
 
 
 }
