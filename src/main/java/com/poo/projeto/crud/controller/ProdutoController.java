a0b1b25a673c563a347064d48af69537d23deae9
Luis Felipe <lfdlprincipal@gmail.com>
2025-08-26 13:52:46 -0300

    feat: Cria método para carregar tabela

--- src/main/java/com/poo/projeto/crud/controller/ProdutoController.java
+++ src/main/java/com/poo/projeto/crud/controller/ProdutoController.java
@@ -1,4 +1,33 @@
 package com.poo.projeto.crud.controller;
 
+import com.poo.projeto.crud.model.Produto;
+import com.poo.projeto.crud.service.ProdutoService;
+import com.poo.projeto.crud.view.ProdutoView;
+
+import java.util.List;
+
 public class ProdutoController {
+
+    private final ProdutoView view;
+    private final ProdutoService produtoService;
+
+
+
+
+    private void carregarTabela() {
+        view.getTableModel().setRowCount(0);
+        List<Produto> produtos = produtoService.listarProdutos();
+        for (Produto p : produtos) {
+
+            view.getTableModel().addRow(new Object[]{
+                    p.getCodigo(), p.getNome(), p.getDescricao(), p.getPreco(), p.getEstoque()
+            });
+        }
+    }
 }
+
+
+
+
+
+
