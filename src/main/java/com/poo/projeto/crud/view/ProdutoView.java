2507bf00374d44ab40d928b594d4dd5496da44b2
Luis Felipe <lfdlprincipal@gmail.com>
2025-08-26 13:10:29 -0300

    feat: Cria campos de textos e botões

--- src/main/java/com/poo/projeto/crud/view/ProdutoView.java
+++ src/main/java/com/poo/projeto/crud/view/ProdutoView.java
@@ -1,4 +1,22 @@
 package com.poo.projeto.crud.view;
 
-public class ProdutoView {
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+public class ProdutoView extends JFrame {
+
+    private final JTextField txtId = new JTextField(5);
+    private final JTextField txtNome = new JTextField(20);
+    private final JTextField txtDescricao = new JTextField(25);
+    private final JTextField txtPreco = new JTextField(10);
+    private final JTextField txtEstoque =new JTextField(10);
+
+    private final JButton btnSalvar = new JButton("Salvar");
+    private final JButton btnAtualizar = new JButton("Atualizar");
+    private final JButton btnDeletar = new JButton("Deletar");
+    private final JButton btnLimpar = new JButton("Limpar Campos");
+
+    private final JTable tabelaProdutos = new JTable();
+    private final DefaultTableModel tableModel;
 }
+
