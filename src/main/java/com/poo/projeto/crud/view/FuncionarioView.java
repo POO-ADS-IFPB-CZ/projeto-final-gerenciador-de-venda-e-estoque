cd24375580f93ec0bcb6bcac0a1a29a846fc5ea3
WesleyyD <delano.wesley@academico.ifpb.edu.br>
2025-08-26 14:57:31 -0300

    feat: Cria botões para gerenciar funcioários

--- src/main/java/com/poo/projeto/crud/view/FuncionarioView.java
+++ src/main/java/com/poo/projeto/crud/view/FuncionarioView.java
@@ -1,9 +1,9 @@
 package com.poo.projeto.crud.view;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 
 public class FuncionarioView {
-
 
     private final JTextField txtId = new JTextField(5);
     private final JTextField txtNome = new JTextField(20);
@@ -11,6 +11,16 @@ public class FuncionarioView {
     private final JTextField txtMatricula = new JTextField(20);
     private final JTextField txtCargo = new JTextField(20);
     private final JTextField txtSalario = new JTextField(10);
+
+    private final JButton btnSalvar = new JButton("Salvar");
+    private final JButton btnAtualizar = new JButton("Atualizar");
+    private final JButton btnDeletar = new JButton("Deletar");
+    private final JButton btnLimpar = new JButton("Limpar");
+
+    private final JTable tabelaFuncionarios = new JTable();
+    private final DefaultTableModel tableModel;
+
+
 
 
 }
