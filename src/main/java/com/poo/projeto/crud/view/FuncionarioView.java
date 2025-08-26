2443dc6687f90ae58dbc4b3798bf19e8053e292a
WesleyyD <delano.wesley@academico.ifpb.edu.br>
2025-08-26 14:40:00 -0300

    feat: Cria campos de txt para view de funcionário

--- src/main/java/com/poo/projeto/crud/view/FuncionarioView.java
+++ src/main/java/com/poo/projeto/crud/view/FuncionarioView.java
@@ -1,8 +1,16 @@
 package com.poo.projeto.crud.view;
 
-public class FuncionarioView {
-    
+import javax.swing.*;
 
+public class FuncionarioView {
+
+
+    private final JTextField txtId = new JTextField(5);
+    private final JTextField txtNome = new JTextField(20);
+    private final JTextField txtCPF = new JTextField(20);
+    private final JTextField txtMatricula = new JTextField(20);
+    private final JTextField txtCargo = new JTextField(20);
+    private final JTextField txtSalario = new JTextField(10);
 
 
 }
